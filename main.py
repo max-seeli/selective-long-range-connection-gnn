0a4ad869f4104085878b514c07cb8bba947b399b
Max Seeliger <maximilian.seeliger@gmail.com>
2023-12-16 17:36:23 +0100

    Adjusted main for better notebook compatibility

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
-from argparse import ArgumentParser
 from attrdict import AttrDict
 
 from experiment import Experiment
 from common import Task, GNN_TYPE, STOP
-
-if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument("--task", dest="task", default=Task.NEIGHBORS_MATCH, type=Task.from_string, choices=list(Task),
-                        required=False)
-    parser.add_argument("--type", dest="type", default=GNN_TYPE.GCN, type=GNN_TYPE.from_string, choices=list(GNN_TYPE),
-                        required=False)
-    parser.add_argument("--dim", dest="dim", default=32, type=int, required=False)
-    parser.add_argument("--depth", dest="depth", default=3, type=int, required=False)
-    parser.add_argument("--num_layers", dest="num_layers", default=None, type=int, required=False)
-    parser.add_argument("--train_fraction", dest="train_fraction", default=0.8, type=float, required=False)
-    parser.add_argument("--max_epochs", dest="max_epochs", default=50000, type=int, required=False)
-    parser.add_argument("--eval_every", dest="eval_every", default=100, type=int, required=False)
-    parser.add_argument("--batch_size", dest="batch_size", default=1024, type=int, required=False)
-    parser.add_argument("--accum_grad", dest="accum_grad", default=1, type=int, required=False)
-    parser.add_argument("--stop", dest="stop", default=STOP.TRAIN, type=STOP.from_string, choices=list(STOP),
-                        required=False)
-    parser.add_argument("--patience", dest="patience", default=20, type=int, required=False)
-    parser.add_argument("--loader_workers", dest="loader_workers", default=0, type=int, required=False)
-    parser.add_argument('--last_layer_fully_adjacent', action='store_true')
-    parser.add_argument('--no_layer_norm', action='store_true')
-    parser.add_argument('--no_activation', action='store_true')
-    parser.add_argument('--no_residual', action='store_true')
-    parser.add_argument('--unroll', action='store_true', help='use the same weights across GNN layers')
-
-    args = parser.parse_args()
-    print(args)
-    Experiment(args).run()
 
 
 def get_fake_args(
@@ -73,3 +44,8 @@ def get_fake_args(
         'no_residual': no_residual,
         'unroll': unroll,
     })
+
+
+def start(params):
+    experiment = Experiment(params)
+    experiment.run()
