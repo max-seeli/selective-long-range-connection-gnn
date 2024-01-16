02e012c73a518ddda7ddc5efe6635f91739cad00
Max Seeliger <maximilian.seeliger@gmail.com>
2024-01-17 00:27:31 +0100

    Experiment can adjusted with CLI params

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from experiment import Experiment
 from common import Task, GNN_TYPE, STOP, LAST_LAYER
-
+import argparse as ap
 import time
 
 def get_fake_args(
@@ -55,19 +55,30 @@ def start(params):
     experiment = Experiment(params)
     experiment.run()
 
-def run(depth, last_layer):
-    params = get_fake_args(
-            max_epochs=50000,
-            depth=depth,
-            last_layer=last_layer,
-            max_samples=32000,
-            learning_rate=0.001,
-            stop=STOP.TRAIN
-    )
+def run(params):
     start_time = time.time()
     start(params)
     end_time = time.time()
     print(f"Total time: {end_time - start_time}")  
 
 if __name__ == '__main__':
-    run(4, LAST_LAYER.FULLY_ADJACENT)
+    args = ap.ArgumentParser()
+    args.add_argument('--type', type=str, default='GCN', choices=['GCN', 'GGNN', 'GIN', 'GAT'], help='GNN type to use')
+    args.add_argument('--max_epochs', type=int, default=50000, help='Maximum number of epochs')
+    args.add_argument('--depth', type=int, default=3, help='Depth of trees from NeighborsMatch dataset')
+    args.add_argument('--last_layer', type=str, default='REGULAR', choices=['REGULAR', 'FULLY_ADJACENT', 'K_HOP'], help='Last layer type')
+    args.add_argument('--max_samples', type=int, default=32000, help='Maximum number of samples to use from NeighborsMatch dataset')
+    args.add_argument('--learning_rate', type=float, default=0.001, help='Learning rate')
+    args.add_argument('--stop', type=str, default='TRAIN', choices=['TRAIN', 'TEST'], help='Stop criterion')
+    args = args.parse_args()
+
+    params = get_fake_args(
+            type=GNN_TYPE.from_string(args.type),
+            max_epochs=args.max_epochs,
+            depth=args.depth,
+            last_layer=LAST_LAYER.from_string(args.last_layer),
+            max_samples=args.max_samples,
+            learning_rate=args.learning_rate,
+            stop=STOP.from_string(args.stop)
+    )
+    run(params)
